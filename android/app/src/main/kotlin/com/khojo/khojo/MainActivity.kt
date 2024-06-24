69cf7f82ccd924637c0f07e5ff4f644c78be1d1e
Prateekgupta07 <prateekgupta.student@gmail.com>
2024-06-24 12:30:04 +0530

    health

---
+++ android/app/src/main/kotlin/com/khojo/khojo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.khojo.khojo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,374 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:os_mime_type/os_mime_type.dart';
+
+void main() {
+  runApp(const GenerativeAISample());
+}
+
+class GenerativeAISample extends StatelessWidget {
+  const GenerativeAISample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter + Generative AI',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.dark,
+          seedColor: const Color.fromARGB(255, 171, 222, 244),
+        ),
+        useMaterial3: true,
+      ),
+      home: const ChatScreen(title: 'Flutter + Generative AI'),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: const ChatWidget());
+  }
+}
+
+class ChatWidget extends StatefulWidget {
+  const ChatWidget({super.key});
+
+  @override
+  State<ChatWidget> createState() => _ChatWidgetState();
+}
+
+class _ChatWidgetState extends State<ChatWidget> {
+  late final GenerativeModel _model;
+  late final ChatSession _chat;
+  final ScrollController _scrollController = ScrollController();
+  final TextEditingController _textController = TextEditingController();
+  final FocusNode _textFieldFocus = FocusNode(debugLabel: 'TextField');
+  bool _loading = false;
+  File? _selectedImage;
+  String mimetype = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _model = GenerativeModel(
+        model: 'gemini-1.5-flash',
+        apiKey: "AIzaSyDyIOpUwLakwsksGQ4VwACF106-P-N17uM",
+        systemInstruction: Content(
+            'user', [TextPart("Check whether its health report or not")]));
+    _chat = _model.startChat();
+  }
+
+  void _scrollDown() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(
+          milliseconds: 750,
+        ),
+        curve: Curves.easeOutCirc,
+      ),
+    );
+  }
+
+  Future<void> _pickImage() async {
+    final pickedFile =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _selectedImage = File(pickedFile.path);
+        mimetype = mimeFromExtension(extension: ".jpg");
+      });
+      if (_selectedImage != null) {
+        _sendMediaChatMessage(
+            "Read the health report and if found not in the range of normal behaviour then suggest how to make it normal");
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final history = _chat.history.toList();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              itemBuilder: (context, idx) {
+                final content = history[idx];
+                final text = content.parts
+                    .whereType<TextPart>()
+                    .map<String>((e) => e.text)
+                    .join('');
+                return MessageWidget(
+                  content: content,
+                  isFromUser: content.role == 'user',
+                );
+              },
+              itemCount: history.length,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 25,
+            ),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () async {
+                    _pickImage();
+                  },
+                  icon: Icon(
+                    Icons.photo,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                Expanded(
+                  child: TextField(
+                    autofocus: true,
+                    focusNode: _textFieldFocus,
+                    decoration:
+                        textFieldDecoration(context, 'Enter a prompt...'),
+                    controller: _textController,
+                    onSubmitted: (String value) {
+                      _sendChatMessage(value);
+                    },
+                  ),
+                ),
+                const SizedBox.square(dimension: 10),
+                if (!_loading)
+                  IconButton(
+                    onPressed: () async {
+                      _sendChatMessage(_textController.text);
+                    },
+                    icon: Icon(
+                      Icons.send,
+                      color: Theme.of(context).colorScheme.primary,
+                    ),
+                  )
+                else
+                  const CircularProgressIndicator(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _sendChatMessage(String message) async {
+    setState(() {
+      _loading = true;
+    });
+
+    try {
+      final response = await _chat.sendMessage(Content.text(message));
+
+      final text = response.text;
+      if (text == null) {
+        _showError('Empty response.');
+        return;
+      } else {
+        setState(() {
+          _loading = false;
+          _scrollDown();
+        });
+      }
+    } catch (e) {
+      _showError(e.toString());
+      setState(() {
+        _loading = false;
+      });
+    } finally {
+      _textController.clear();
+      setState(() {
+        _loading = false;
+      });
+      _textFieldFocus.requestFocus();
+    }
+  }
+
+  Future<void> _sendMediaChatMessage(String message) async {
+    setState(() {
+      _loading = true;
+    });
+
+    try {
+      List<Part> parts = [TextPart(message)];
+      if (_selectedImage != null) {
+        Uint8List imageData = await _selectedImage!.readAsBytes();
+        parts.add(DataPart(mimetype, imageData));
+      }
+
+      final response = await _chat.sendMessage(Content.multi(parts));
+      final text = response.text;
+
+      if (text == null) {
+        _showError('Empty response.');
+        return;
+      } else {
+        setState(() {
+          _loading = false;
+          _scrollDown();
+        });
+      }
+    } catch (e) {
+      _showError(e.toString());
+      setState(() {
+        _loading = false;
+      });
+    } finally {
+      _textController.clear();
+      setState(() {
+        _loading = false;
+      });
+      _textFieldFocus.requestFocus();
+    }
+  }
+
+  void _showError(String message) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Something went wrong'),
+          content: SingleChildScrollView(
+            child: Text(message),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
+
+class MessageWidget extends StatelessWidget {
+  const MessageWidget({
+    super.key,
+    required this.content,
+    required this.isFromUser,
+  });
+
+  final Content content;
+  final bool isFromUser;
+
+  @override
+  Widget build(BuildContext context) {
+    final textParts = content.parts.whereType<TextPart>().toList();
+    final imageParts = content.parts.whereType<DataPart>().toList();
+
+    return Row(
+      mainAxisAlignment:
+          isFromUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+      children: [
+        Flexible(
+          child: Container(
+            constraints: const BoxConstraints(maxWidth: 480),
+            decoration: BoxDecoration(
+              color: isFromUser
+                  ? Theme.of(context).colorScheme.primaryContainer
+                  : Theme.of(context).colorScheme.onInverseSurface,
+              borderRadius: BorderRadius.circular(18),
+            ),
+            padding: const EdgeInsets.symmetric(
+              vertical: 15,
+              horizontal: 20,
+            ),
+            margin: const EdgeInsets.only(bottom: 8),
+            child: Column(
+              crossAxisAlignment: isFromUser
+                  ? CrossAxisAlignment.end
+                  : CrossAxisAlignment.start,
+              children: [
+                if (textParts.isNotEmpty)
+                  ...textParts
+                      .map((textPart) => MarkdownBody(data: textPart.text)),
+                if (imageParts.isNotEmpty)
+                  ...imageParts.map((imagePart) {
+                    return Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Image.memory(
+                        imagePart.bytes,
+                        fit: BoxFit.contain,
+                      ),
+                    );
+                  }),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+InputDecoration textFieldDecoration(BuildContext context, String hintText) =>
+    InputDecoration(
+      contentPadding: const EdgeInsets.all(15),
+      hintText: hintText,
+      border: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(14),
+        ),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          Radius.circular(14),
+        ),
+        borderSide: BorderSide(
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
+    );
+
+// class DataPart implements Part {
+//   final String mimeType;
+//   final Uint8List _data;
+//
+//   DataPart(this.mimeType, this._data);
+//
+//   Uint8List get data => _data;
+//
+//   @override
+//   String get type => 'data';
+//
+//   @override
+//   Map<String, dynamic> toJson() => {
+//     'mimeType': mimeType,
+//     'data': _data,
+//   };
+// }
